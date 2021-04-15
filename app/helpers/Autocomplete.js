7d4fd5582fb7105b52e27e41b319d1778b4d57be
JaroslavMerta <log.merta@gmail.com>
2021-04-16 00:20:12 +0200

    vytvoreni tridy Autocomplete a jejiho konstruktoru

---
+++ app/helpers/Autocomplete.js
@@ -0,0 +1,7 @@
+export class WeatherForecast {
+
+    constructor (){
+        this.button = document.getElementById("b-city_name");
+        this.myInput = document.getElementById("myInput");
+    }
+}

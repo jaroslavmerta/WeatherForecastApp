2a93beb154f8a02186c529932e4ce316270ebeb0
JaroslavMerta <log.merta@gmail.com>
2021-04-16 12:50:28 +0200

    vytvoreni tridy WeatherForecast a jejiho konstruktoru

---
+++ app/WeatherForecast/WeatherForecast.js
@@ -0,0 +1,9 @@
+'use strict'
+export class WeatherForecast {
+
+    constructor (){
+        this.button = document.getElementById("b-city_name");
+        this.myInput = document.getElementById("myInput");
+    }
+    
+}

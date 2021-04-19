9b326ddf6b64bb869cb43bb8d14f2ce66690a670
JaroslavMerta <log.merta@gmail.com>
2021-04-19 11:16:12 +0200

    pridani anotaci do tridy Ajax

--- app/helpers/Ajax.js
+++ app/helpers/Ajax.js
@@ -2,6 +2,11 @@
 
 export class Ajax {
 
+    /**
+     * Performs a GET request to a given URL and returns the result
+     * @param {string} url The URL address to send the request to
+     * @returns {Promise<Response>}
+     */
     static async fetchToJSON(url){
         let response = await fetch(url);
         if (!response.ok)

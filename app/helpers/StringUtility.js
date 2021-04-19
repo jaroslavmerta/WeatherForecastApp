8cfb9b14826de21e4f3d2f95c3746a61d33518e5
JaroslavMerta <log.merta@gmail.com>
2021-04-19 11:34:53 +0200

    pridani anotaci do tridy StringUtility

--- app/helpers/StringUtility.js
+++ app/helpers/StringUtility.js
@@ -2,9 +2,13 @@
 
 export class StringUtility {
   
-    //pokud je v předávaném řetězci čárka, rozdělí řetězec na slova podle čárky
-    //, odstraní mezery ze začátku a konce slova a uloží ji do pole
-    //pokud řetězec nemá čárku, odstraní jen mezery ze začátku a konce řetězce
+    /**
+     * Checks for separator in a string
+     * @param {string} string The string to be checked for the presence of a separator
+     * @param {string} sep The separator to search for
+     * @returns {array} if the separator was founded 
+     * @returns {string} if the separator was not founded
+     */
     static checkString(string, sep){
         let attributes;
         if (string.includes(sep)){

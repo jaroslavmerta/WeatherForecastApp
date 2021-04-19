31339166b514a5553bd3d3a0c1d693034094ed40
JaroslavMerta <log.merta@gmail.com>
2021-04-19 12:15:57 +0200

    pridani anotaci ve tride TimeUtility

--- app/helpers/TimeUtility.js
+++ app/helpers/TimeUtility.js
@@ -2,36 +2,47 @@
 
 export class TimeUtility {
 
-    static get days() {
-        return ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So'];
-    }
-
-    //změna názvu z loadToday
-    //vrací aktuální čas a vrací den v týdnu jako číslo
+    /**Returns today numerically expressed as the day of the week
+     * @returns {number}
+     */
     static getToday() {
         let date = new Date();
         return date.getDay();
     }
 
-    static dayByWord = (dayOfWeek) =>{
-        return this.days[dayOfWeek];
-    }
-
     //převádí unixový čas na standardní a vrací den v týdnu jako číslo
+    /**
+     * Converts UNIX timestam into javascript Date object
+     * @param {number} unixTimeStamp UNIX timestamps in seconds
+     * @returns {number}
+     */
     static timeConverter(unixTimeStamp){
         let date = new Date(unixTimeStamp * 1000);
         return date.getDay();
     }
 
+    /**
+     * Converts UNIX timestam into javascript Date object
+     * @param {number} unixTimeStamp UNIX timestamps in seconds
+     * @returns {object}
+     */
     static DateObjectFromUTS(unixTimeStamp){
         return new Date(unixTimeStamp * 1000);
     }
 
+    /**
+     * Creates today formatted into an abbreviated word form according to the browser language
+     * @returns {string}
+     */
     static toLocalTodayFormat(){
         let date = new Date();
         return date.toLocaleDateString([], { weekday: 'short' });
     }
 
+    /**
+     * Creates fake Date objects to get the specific time of the day formatted according to the browser language
+     * @returns {object}
+     */
     static fakeTimeObjects(){
         let date=[];
         let localTime=[];

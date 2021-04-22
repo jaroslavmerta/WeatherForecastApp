2e4e1dfc80ec432b186eb19e3b71325a651363a4
JaroslavMerta <log.merta@gmail.com>
2021-04-22 17:25:43 +0200

    vytvoreni funkce ve tride CityAutocomplete

--- app/WeatherForecast/CityAutocomplete.js
+++ app/WeatherForecast/CityAutocomplete.js
@@ -1,6 +1,20 @@
 'use strict'
 import { Autocomplete } from '../Autocomplete/Autocomplete.js';
 
-class CityAutocomplete extends Autocomplete {
-    
+export class CityAutocomplete extends Autocomplete {
+
+  /**
+   * Creates item list with a city name and country
+   * @param {object} city City data
+   * @returns {object}
+   */
+  _generateListItem(city){
+    let div;
+    div = document.createElement("DIV");
+    div.setAttribute('class', "autocomplete-item")
+    div.innerHTML = `<strong> ${city.name}, ${city.country} </strong>`;
+    div.innerHTML += "<input type='hidden' value='" + city.name + 
+    ", " + city.country + "'>";
+    return div;
+  }    
 }
